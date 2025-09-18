5e3a4b5688deacaa09912624d85d5378ac9ec426
angelqtypie <angeljustineballaso07@gmail.com>
2025-09-18 22:31:50 +0800

    Utro wapahaman

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,6 +10,8 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import React from "react";
+
 import { useHistory } from "react-router-dom";
 import { supabase } from "../utils/supabaseClient";
 
