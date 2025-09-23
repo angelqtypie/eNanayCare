df125c5fd50846dccfb01b8d605dfce1a00c0f27
angelqtypie <angeljustineballaso07@gmail.com>
2025-09-23 23:41:15 +0800

    Updates

--- assets/swipe-back-legacy-Sv699jbM.js
+++ assets/swipe-back-legacy-Sv699jbM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CVUZQDRy.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BN2N31AE.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
