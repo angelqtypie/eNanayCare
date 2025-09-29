2c0272a145651b35fe61debde044d8395c4183b2
angelqtypie <angeljustineballaso07@gmail.com>
2025-09-29 13:17:54 +0800

    Updates

--- assets/ios.transition-legacy-Bgb_g44-.js
+++ assets/ios.transition-legacy-Bgb_g44-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DC0Ut_7Z.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DoVUQn3m.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
