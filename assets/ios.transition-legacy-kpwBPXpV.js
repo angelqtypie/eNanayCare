cb60fe36598e3d5d7f10a16bc016f9c3647964e7
angelqtypie <angeljustineballaso07@gmail.com>
2025-10-07 21:53:37 +0800

    Updates

--- assets/ios.transition-legacy-kpwBPXpV.js
+++ assets/ios.transition-legacy-kpwBPXpV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dakmk37H.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Byo0ShIi.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
