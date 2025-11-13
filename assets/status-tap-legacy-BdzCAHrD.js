1ba514854c9f59fe4a8d6b15f8b74a765616168b
angelqtypie <angeljustineballaso07@gmail.com>
2025-11-13 11:01:29 +0800

    Updates

---
+++ assets/status-tap-legacy-BdzCAHrD.js
@@ -0,0 +1,5 @@
+System.register(["./index-legacy-D48QYD5H.js","jspdf","jspdf-autotable"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s},null,null],execute:function(){
+/*!
+             * (C) Ionic http://ionicframework.com - MIT License
+             */
+e("startStatusTap",()=>{const e=window;e.addEventListener("statusTap",()=>{n(()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const u=r(a);u&&new Promise(e=>s(u,e)).then(()=>{o(async()=>{u.style.setProperty("--overflow","hidden"),await i(u,300),u.style.removeProperty("--overflow")})})})})})}}});
